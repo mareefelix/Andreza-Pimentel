ec559894159a83e8b1b8524b5331ec7f70b7a264
mareefelix <mareefelixcontact@gmail.com>
2026-05-19 18:08:58 -0300

    feat: Setup Andreza Pimentel application
    Configure application metadata, update dependencies, and integrate TanStack Router for navigation. This change establishes the foundational structure for the application's routing and includes essential project details.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,12 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import { RouterProvider } from "@tanstack/react-router";
+import { getRouter } from "./router";
+
+const router = getRouter();
+
 export default function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
+
